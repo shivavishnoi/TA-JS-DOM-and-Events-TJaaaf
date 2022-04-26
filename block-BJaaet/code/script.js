7ddd91b3062e5b033db35a05ee433e7b55b4578f
shivaji <shivavishnoi.vishnoi@gmail.com>
2022-04-26 09:54:14 +0530

    movie list

--- block-BJaaet/code/script.js
+++ block-BJaaet/code/script.js
@@ -1,5 +1,34 @@
-let input = document.querySelector(`input`)
+let form = document.querySelector(`form`)
 
-input.addEventListener(`input`, (e) => {
-  console.log(e.target.value)
-})
+let movies = []
+
+function handleSubmit(e) {
+  e.preventDefault()
+  let movieName = e.target.elements.movieInput.value
+  movies.push(movieName)
+  addMovie(movieName)
+  e.target.elements.movieInput.value = ``
+}
+form.addEventListener(`submit`, handleSubmit)
+
+
+function addMovie(name) {
+  let html = `<div class="moviename">${name}</div>
+              <div class="close">Close</div>`
+  let movieBoxElm = document.createElement(`div`)
+  movieBoxElm.classList.add(`moviebox`)
+  movieBoxElm.innerHTML = html
+  document.querySelector(`.movielist`).append(movieBoxElm)
+}
+
+// let close = document.querySelector(`.close`)
+// close.addEventListener(`click`, (e) => {
+//   close.parentElement.remove()
+// })
+
+
+
+  //       < div class="moviebox" >
+  //         <div class="moviename">Raja Hindustani</div>
+  //         <div class="close">Close</div>
+  //       </div >
